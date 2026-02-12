39fb6474f9de927981461c91cfe16e1572be9319
MuzaffarOrtiqov <muzaffarmike2@gmail.com>
2026-02-12 11:55:05 +0000

    API was configured for front

--- giybat-frontend-verstka-master/js/AppConfig.js
+++ giybat-frontend-verstka-master/js/AppConfig.js
@@ -1,5 +1,5 @@
 export default class AppConfig {
     static API = window.location.hostname === 'localhost'
         ? "http://localhost:8080"
-        : "http://13.60.45.252:8080";
+        : "http://api.giybatnoma.uz:8080";
 }
